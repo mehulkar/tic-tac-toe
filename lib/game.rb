ddb02b1078489b214601d6c5896b4ced5511f4d8
rakm <rakm@rakm.io>
2014-09-13 14:19:29 -0700

    extract invalid move logic to private methods

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,21 @@ class Game
   end
 
   def play(spot, sign)
-    raise StandardError if !["X", "O"].include? sign
-    raise StandardError if (spot < 1 || spot > @moves.length)
+    raise StandardError if invalid_move?(sign, spot)
     @moves[spot - 1] = sign
+  end
+
+private
+
+  def invalid_move?(sign, spot)
+    invalid_sign?(sign) || invalid_spot?(spot)
+  end
+
+  def invalid_sign?(sign)
+    !["X", "O"].include? sign
+  end
+
+  def invalid_spot?(spot)
+    spot < 1 || spot > @moves.length
   end
 end
